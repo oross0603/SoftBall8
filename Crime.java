0382064093e42a801addf01da3d5e6b14d7e4d10
Omar Ross <oross@missouriwestern.edu>
2017-06-01 19:05:57 -0500

    Enter a title

--- Crime.java
+++ Crime.java
@@ -1,8 +1,30 @@
 package com.csc445.omie.softball;
 
+import java.util.UUID;
+
 /**
  * Created by Omie on 6/1/2017.
  */
 
 public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        // Generates unique identifier
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getID() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
 }
